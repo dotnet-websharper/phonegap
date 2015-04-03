a798a353454eac107189ea44dc68d54d93977dd9
IntelliFactory <bitbucket@intellifactory.com>
2015-04-03 16:14:29 +0000

    Removing IntelliFactory from the references and namespaces

--- tools/build.fsx
+++ tools/build.fsx
@@ -1,19 +1,19 @@
 #if LOCAL
-#r "../../websharper.typescript/build/net40/IntelliFactory.WebSharper.TypeScript.dll"
+#r "../../websharper.typescript/build/net40/WebSharper.TypeScript.dll"
 #else
-#r "../packages/WebSharper.TypeScript/tools/net40/IntelliFactory.WebSharper.TypeScript.dll"
+#r "../packages/WebSharper.TypeScript/tools/net40/WebSharper.TypeScript.dll"
 #endif
 
 open System
 open System.IO
-module C = IntelliFactory.WebSharper.TypeScript.Compiler
+module C = WebSharper.TypeScript.Compiler
 
 let local args =
     Path.Combine(__SOURCE_DIRECTORY__, Path.Combine(Array.ofList args))
 
 let dts = local [".."; "node_modules"; "typedphonegap"; "build"; "TypedPhoneGap.d.ts"]
 let mjs = local [".."; "node_modules"; "typedphonegap"; "build"; "TypedPhoneGap.min.js"]
-let lib = local [".."; "packages"; "WebSharper.TypeScript.Lib"; "lib"; "net40"; "IntelliFactory.WebSharper.TypeScript.Lib.dll"]
+let lib = local [".."; "packages"; "WebSharper.TypeScript.Lib"; "lib"; "net40"; "WebSharper.TypeScript.Lib.dll"]
 let snk = local [Environment.GetEnvironmentVariable("INTELLIFACTORY"); "keys"; "IntelliFactory.snk"]
 
 let fsCore =
@@ -24,10 +24,10 @@ let fsCore =
 
 let opts =
     {
-        C.Options.Create("IntelliFactory.WebSharper.PhoneGap", [dts]) with
+        C.Options.Create("WebSharper.PhoneGap", [dts]) with
             AssemblyVersion = Some (Version "3.0.0.0")
             EmbeddedResources = [C.EmbeddedResource.FromFile(mjs).AsWebResource("text/javascript")]
-            Renaming = IntelliFactory.WebSharper.TypeScript.Renaming.RemovePrefix "TypedPhoneGap"
+            Renaming = WebSharper.TypeScript.Renaming.RemovePrefix "TypedPhoneGap"
             References = [C.ReferenceAssembly.File lib; C.ReferenceAssembly.File fsCore]
             StrongNameKeyFile = Some snk
             Verbosity = C.Level.Verbose
@@ -43,7 +43,7 @@ for msg in result.Messages do
 match result.CompiledAssembly with
 | None -> ()
 | Some asm ->
-    let out = local [".."; "build"; "IntelliFactory.WebSharper.PhoneGap.dll"]
+    let out = local [".."; "build"; "WebSharper.PhoneGap.dll"]
     let dir = DirectoryInfo(Path.GetDirectoryName(out))
     if not dir.Exists then
         dir.Create()
